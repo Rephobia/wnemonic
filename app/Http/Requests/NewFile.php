c4aa20c131db7055c0446653d24c026e50be8b33
Besofemus <26533256+Besofemus@users.noreply.github.com>
2020-01-27 20:02:17 +0300

    Add CheckName custom request

--- app/Http/Requests/NewFile.php
+++ app/Http/Requests/NewFile.php
@@ -3,9 +3,10 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+
 use App\Rules\IsFile;
 use App\Rules\UniqueFile;
-
+use App\Utils\Constants\FileName;
 
 class NewFile extends FormRequest
 {
@@ -18,16 +19,16 @@ class NewFile extends FormRequest
     {
         return true;
     }
-
+    
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules()
+    public function rules() : array
     {
-        return [
-            "name" => ["required", new IsFile($this), new UniqueFile]
-        ];
+        $extendRules = FileName::rules(new IsFile($this), new UniqueFile);
+        $rules = array(FileName::nameField() => $extendRules);
+        return $rules;
     }
 }
