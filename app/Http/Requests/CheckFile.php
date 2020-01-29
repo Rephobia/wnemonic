623206bfd228ecdffea34f3354f7ca7d0d985a05
Besofemus <26533256+Besofemus@users.noreply.github.com>
2020-01-29 20:28:56 +0300

    Rename CheckName to CheckFile

--- app/Http/Requests/CheckFile.php
+++ app/Http/Requests/CheckFile.php
@@ -6,7 +6,7 @@ use Illuminate\Foundation\Http\FormRequest;
 
 use App\Literal;
 
-class CheckName extends FormRequest
+class CheckFile extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
