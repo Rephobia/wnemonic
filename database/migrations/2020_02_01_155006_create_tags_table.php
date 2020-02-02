5ce72fd9c3c81dda6ced9f16a37c3caa64e23ca0
DoomerRoman <doomerroman@gmail.com>
2020-02-02 20:06:00 +0300

    Add tag model

--- database/migrations/2020_02_01_155006_create_tags_table.php
+++ database/migrations/2020_02_01_155006_create_tags_table.php
@@ -14,10 +14,22 @@ class CreateTagsTable extends Migration
      */
     public function up()
     {
-        Schema::create('tags', function (Blueprint $table) {
+        Schema::create('tags', function (Blueprint $table)
+        {
             $table->bigIncrements('id');
             $table->timestamps();
             $table->string(Literal::tagField())->unique();
+        });
+
+        Schema::create('file_tags', function (Blueprint $table)
+        {
+            $table->timestamps();
+            $table->bigInteger('file_id');
+            $table->foreign('file_id')->references('id')->on('files')->onDelete('cascade');
+
+            $table->bigInteger('tags_id');
+            $table->foreign('tags_id')->references('id')->on('tags')->onDelete('cascade');
+ 
         });
     }
 
@@ -28,6 +40,7 @@ class CreateTagsTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('file_tags');
         Schema::dropIfExists('tags');
     }
 }
