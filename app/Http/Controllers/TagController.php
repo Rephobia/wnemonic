641b18d7e4f86cab75959874e328cf3e182b9dcc
DoomerRoman <doomerroman@gmail.com>
2020-02-06 20:26:02 +0300

    Separate Repository::updateTags from Repository::rename
    Repository is friend FileView and can access to data field.
    Repository methods must be work with FileView objects, instead
    strings

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -7,12 +7,16 @@ use App\Repository;
 
 class TagController extends Controller
 {
-
+    public function __construct(Repository $repository)
+    {
+        $this->repository = $repository;
+    }
     public function show(string $tags)
     {
-        $files = Repository::all($tags);
+        $files = $this->repository->all($tags);
                 
         return view("main")->with("files", $files);
     }
+    private $repository;
 
 }
--- app/Http/Requests/BasicRequest.php
+++ app/Http/Requests/BasicRequest.php
@@ -6,7 +6,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class BasicRequest extends FormRequest
 {
-
     /**
      * Determine if the user is authorized to make this request.
      *
