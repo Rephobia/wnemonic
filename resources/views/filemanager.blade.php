5b4412ef27f3871194e08e2169c885c101ecaff7
DoomerRoman <doomerroman@gmail.com>
2020-02-09 20:37:22 +0300

    Change file render style

--- resources/views/filemanager.blade.php
+++
@@ -1,4 +0,0 @@
-<a href={{ $file->link() }} ><button>Raw Link</button></a>
-<a href={{ $file->link() }} download={{ $file->name() }}><button>Download</button></a>
-<a href={{ url("edit/".$file->name()) }}><button>Edit</button></a>
-
