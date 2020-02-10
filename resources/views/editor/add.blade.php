15d57dc839ffc793c6ca955951b049ba8943f8e0
DoomerRoman <doomerroman@gmail.com>
2020-02-10 13:29:13 +0300

    Change control buttons style

--- resources/views/editor/add.blade.php
+++ resources/views/editor/add.blade.php
@@ -2,11 +2,21 @@
 
 @section("content")
 
-    <form action="/add" method="post" enctype="multipart/form-data">
-	@csrf
-	<input type="file" name={{ \App\Literal::nameField() }}>
-	<br>
-	@include("editor/control")
-    </form>
+    <div class="editor">
+	
+	<form  method="post" enctype="multipart/form-data">
+	    @csrf
+	    <input class="control" type="file" name={{ \App\Literal::nameField() }}>
+	    <input type="text" name={{ \App\Literal::tagField() }}>
+	    
+	    <div class="control">
+		<button type="reset">Reset</button>
+		<button type="submit" formaction="/add">Save</button>
+		<button type="submit" formaction="/cancel">Cancel</button>
+	    </div>
+	    
+	</form>
+	
+    </div>
     
 @endsection
