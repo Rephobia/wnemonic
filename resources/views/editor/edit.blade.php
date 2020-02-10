17ab42b71c2d14b0d5bbba8ee230a7248e25f5af
Rephobia <26533256+Rephobia@users.noreply.github.com>
2020-02-10 13:29:13 +0300

    Change control buttons style

--- resources/views/editor/edit.blade.php
+++ resources/views/editor/edit.blade.php
@@ -2,17 +2,26 @@
 
 @section("content")
 
-    <form method="post">
-	@csrf
+    <div class="editor">
+	
+	<form method="post">
+	    @csrf
 
-	<input type="hidden" name={{ \App\Literal::nameField() }} value={{ $file->name() }}>
-	<br>
-	<label>Name:</label>
-	<input type="text" name={{ \App\Literal::newnameField() }} value={{ $file->name() }}>
-	<br>
-	@include("editor/control")
-	<button type="submit" formaction="/delete" style="color:red">Delete</button>
-
-    </form>
+	    <input type="hidden" name={{ \App\Literal::nameField() }} value={{ $file->name() }}>
+	    <br>
+	    <label>Name:</label>
+	    <input type="text" name={{ \App\Literal::newnameField() }} value={{ $file->name() }}>
+	    
+	    <div class="control">
+		<input type="text" name={{ \App\Literal::tagField() }} value={{ $file->tagsString() }}>
+		<button type="reset">Reset</button>
+		<button type="submit" formaction="/edit">Save</button>
+		<button type="submit" formaction="/cancel">Cancel</button>
+		<button class="delete" type="submit" formaction="/delete">Delete</button>
+	    </div>
+	    
+	</form>
+	
+    </div>
 
 @endsection
