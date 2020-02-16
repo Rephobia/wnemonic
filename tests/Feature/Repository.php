45b4bc6e1ea47900654e0a053de6dc560f27d08b
Rephobia <26533256+Rephobia@users.noreply.github.com>
2020-02-16 13:53:54 +0300

    Add Seeder class for testing

--- tests/Feature/Repository.php
+++ tests/Feature/Repository.php
@@ -1,9 +1,28 @@
 <?php
 
+/*
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+
+ * Copyright (C) 2020 Roman Erdyakov
+
+ * This file is part of Wnemonic. It is a tag based file manager.
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
 namespace Tests\Feature;
-
-use Illuminate\Http\UploadedFile;
-
 
 class Repository extends \Tests\TestCase
 {    
@@ -14,12 +33,13 @@ class Repository extends \Tests\TestCase
      */
     public function writeFile() : void
     {
-        $kilobytes = 1024;
+        $fileName = "test_file";
+        $tags = "test_tag";
         
-        $file = UploadedFile::fake()->create("testfiles", $kilobytes);
-        $repository = $this->app->make(\App\Repository::class);
-        $fileView = $repository->save($file, "tag");
-
-        \Storage::assertExists($fileView->path());
+        $filesystem = \Storage::fake("testing_filesystem");
+        
+        $fileView = Seeder::seed($fileName, $tags, $filesystem);
+        
+        $filesystem->assertExists($fileView->path());
     }
 }
