103ea5aad51638a2ff57caab98d46c06ab07a668
Besofemus <26533256+Besofemus@users.noreply.github.com>
2020-03-02 16:56:29 +0300

    Add search bar

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,2 +1,9 @@
 <a href={{ url("/") }}>Wnemonic</a>
 <a href={{ url("add") }}>Add</a>
+
+<form  method="POST" action="/search">
+    @csrf
+    <div class="search-bar">
+	<input type="text" name={{ \App\Literal::searchField() }}>
+    </div>
+</form>
